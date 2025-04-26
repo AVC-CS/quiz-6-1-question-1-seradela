6c6b6b7b8078ac580f41b53756f1d2a3fe434bd1
seradela <serapiodela@gmail.com>
2025-04-25 18:15:43 -0700

    6-1-Q2 done

--- main.hpp
+++ main.hpp
@@ -1,8 +1,45 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
 
-//********************
-// Declare Function Prototypes
-//********************
+#include <iostream>
+using namespace std;
 
-// ******************************
-// Implement all your functions here
-// ******************************
+// Get two integer values from the user such that begin < end
+void getTwoValues(int &begin, int &end) {
+    do {
+        cout << "Enter two integers (begin < end): ";
+        cin >> begin >> end;
+        if (begin >= end) {
+            cout << "Invalid input. 'begin' must be less than 'end'. Try again.\n";
+        }
+    } while (begin >= end);
+}
+
+// Helper function to check if a number is prime
+bool isPrime(int num) {
+    if (num < 2) return false;
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+// Return the next prime number after the given "begin"
+int getNextPrime(int begin) {
+    int next = begin + 1;
+    while (!isPrime(next)) {
+        ++next;
+    }
+    return next;
+}
+
+// Return the previous prime number before the given "end"
+int getPrevPrime(int end) {
+    int prev = end - 1;
+    while (prev > 1 && !isPrime(prev)) {
+        --prev;
+    }
+    return prev;
+}
+
+#endif
